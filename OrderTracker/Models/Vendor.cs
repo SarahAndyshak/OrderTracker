b7d332eb6c8ace9392b6ce513c5801a44d42de78
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-03-03 08:50:22 -0800

    vendor constructor test passed

---
+++ OrderTracker/Models/Vendor.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace OrderTracker.Models
+{
+  public class Vendor
+  {
+    public string VendorName { get; set; }
+    public Vendor(string vendorName)
+    {
+      VendorName = vendorName;
+    }
+  }
+}
