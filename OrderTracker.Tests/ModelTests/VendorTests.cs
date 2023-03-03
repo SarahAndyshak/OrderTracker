27a86aa3ac4641c6e1d5c61709aad1ef005f5448
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-03-03 08:47:55 -0800

    test vendor constructor failed as expected.

---
+++ OrderTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace OrderTracker.Test
+{
+  [TestClass]
+  public class VendorTests
+  {
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("test vendor");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+  
+}
