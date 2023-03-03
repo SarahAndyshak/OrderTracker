5c45365267458d24a523a9e8ad0e8b5adda4857f
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-03-03 10:40:40 -0800

    All Order tests passing, found mistakes in first test and some declarations in Order.cs

--- OrderTracker/Models/Order.cs
+++ OrderTracker/Models/Order.cs
@@ -5,9 +5,24 @@ namespace OrderTracker.Models
   public class Order
   {
     public string OrderTitle { get; set; }
+    public int Id { get; }
+    private static List<Order> _instances = new List<Order> { };
+
     public Order(string orderTitle)
     {
       OrderTitle = orderTitle;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
   }
 }
